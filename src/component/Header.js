e0eea9478a9356f5f603955bb83e85d33ec6ea22
Roman Lesnoy <romanlesnoy@outlook.com>
2022-05-10 19:00:59 +0300

    Add header component

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Hacker News</h1>
+            <button>Update</button>
+        </header>
+    );
+};
+
+export default Header;
