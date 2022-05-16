2034d843f1e9a27d511e579aa6dcbf8e402e1687
Roman Lesnoy <romanlesnoy@outlook.com>
2022-05-16 15:55:29 +0300

    Add Preloader

--- src/component/NewsList.js
+++ src/component/NewsList.js
@@ -4,23 +4,27 @@ import NewsCard from "./NewsCard";
 import { useSelector } from "react-redux";
 
 const NewsList = () => {
-    const stories = useSelector((state) => state.news.story);
+    const stories = useSelector((state) => state.news.news);
     const isLoading = useSelector((state) => state.news.isLoading);
 
     return (
-        <ul>
-            {isLoading &&
-                stories.map((story) => (
-                    <NewsCard
-                        key={story.id}
-                        title={story.title}
-                        rating={story.score}
-                        author={story.by}
-                        date={story.time}
-                        url={story.url}
-                    />
-                ))}
-        </ul>
+        <React.Fragment>
+            {isLoading && <p>Loading...</p>}
+            <ul>
+                {!isLoading &&
+                    stories.map((story) => (
+                        <NewsCard
+                            key={story.id}
+                            id={story.id}
+                            title={story.title}
+                            rating={story.score}
+                            author={story.by}
+                            date={story.time}
+                            url={story.url}
+                        />
+                    ))}
+            </ul>
+        </React.Fragment>
     );
 };
 
