df988a44a95d8f1a1d9282ddcd83ac28dc258cb5
Roman Lesnoy <romanlesnoy@outlook.com>
2022-05-18 15:56:33 +0300

    Add data update functionality for the button

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
+import { fetchNews } from "../store/news-actions";
 import styles from "./Header.module.css";
+import Button from "./Button";
 
 const Header = () => {
+    const dispatch = useDispatch();
+
+    const updateNews = () => {
+        dispatch(fetchNews());
+        console.log("CLICK");
+    };
+
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>Hacker News</h1>
-            <button className={styles.button}>Update</button>
+            <Button onClick={updateNews} text="Update" />
         </header>
     );
 };
