f97e0e5a5b3aa99a6e2045d0d931e65d4fa49015
Roman Lesnoy <romanlesnoy@outlook.com>
2022-05-18 15:57:00 +0300

    Add go back functionality for the button

--- src/component/ArticlePage.js
+++ src/component/ArticlePage.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
-
+import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import styles from "./ArticlePage.module.css";
 import Article from "./Article";
 import Preloader from "./Preloader";
+import Button from "./Button";
 import { newsActions } from "../store/news-slice";
 import { fetchStory } from "../store/news-actions";
 
 const ArticlePage = () => {
+    const history = useHistory();
     const { id } = useParams();
     const dispatch = useDispatch();
     const stories = useSelector((state) => state.news.news);
@@ -28,7 +30,7 @@ const ArticlePage = () => {
     return (
         <React.Fragment>
             <header className={styles.header}>
-                <button className={styles.button}> Back </button>
+                <Button onClick={() => history.goBack()} text={"Back"} />
             </header>
             <main>
                 {isLoading && !hasArticle && <Preloader />}
