a2047113a2b328d3c666dd893240d5e081b6fdd3
Roman Lesnoy <romanlesnoy@outlook.com>
2022-05-31 15:24:02 +0300

    Make CommentList component logic more flexible

--- src/component/CommentsList.js
+++ src/component/CommentsList.js
@@ -1,43 +1,25 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import PropTypes from "prop-types";
 
 import styles from "./CommentList.module.css";
 import Comment from "./Comment";
-import { fetchComments } from "../store/news-actions";
 
-const CommentsList = () => {
-    const dispatch = useDispatch();
-    const comments = useSelector((state) => state.news.comments);
-    const article = useSelector((state) => state.news.article);
-    const hasComments = useSelector((state) => state.news.hasComments);
-
-    const { kids } = article;
-
-    useEffect(() => {
-        if (kids) {
-            dispatch(fetchComments(kids));
-        }
-    }, [dispatch, kids]);
-
+const CommentsList = ({ comments }) => {
     return (
         <React.Fragment>
-            {!hasComments && <p>No comments</p>}
-            {hasComments && (
-                <ul>
-                    {comments.map((item) => (
-                        <li key={item.id} className={styles.comment}>
-                            <Comment item={item} />
-                        </li>
-                    ))}
-                </ul>
-            )}
+            <ul>
+                {comments.map((item) => (
+                    <li key={item.id} className={styles.comment}>
+                        <Comment item={item} />
+                    </li>
+                ))}
+            </ul>
         </React.Fragment>
     );
 };
 
 CommentsList.propTypes = {
-    kids: PropTypes.array
+    comments: PropTypes.array
 };
 
 export default CommentsList;
