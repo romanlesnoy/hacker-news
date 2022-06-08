65006992595ba30f30f941084903165ce5fe7ca7
Roman Lesnoy <romanlesnoy@outlook.com>
2022-06-08 14:36:33 +0300

    Change rules for sanitize-html

--- src/helpers/sanitizeHtml.js
+++ src/helpers/sanitizeHtml.js
@@ -3,7 +3,7 @@ import sanitizeHtml from "sanitize-html";
 const rules = {
     allowedTags: ["b", "i", "em", "strong", "a", "pre", "div", "code"],
     transformTags: {
-        pre: "div",
+        pre: "p",
         a: sanitizeHtml.simpleTransform("a", {
             target: "_blank",
             rel: "noopener noreferrer"
