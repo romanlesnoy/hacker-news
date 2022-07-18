c3f28d86c3e73e46ef89e84a3aec652568640966
Roman Lesnoy <romanlesnoy@outlook.com>
2022-07-18 16:30:32 +0300

    Add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import MainPage from "./pages/MainPage/MainPage";
-import ArticlePage from "./pages/ArticlePage/ArticlePage";
-import NotFound from "./pages/NotFound/NotFound";
+// import MainPage from "./pages/MainPage/MainPage";
+// import ArticlePage from "./pages/ArticlePage/ArticlePage";
+// import NotFound from "./pages/NotFound/NotFound";
+import Preloader from "./components/Preloader/Preloader";
+
+const MainPage = React.lazy(() => import("./pages/MainPage/MainPage"));
+const ArticlePage = React.lazy(() => import("./pages/ArticlePage/ArticlePage"));
+const NotFound = React.lazy(() => import("./pages/NotFound/NotFound"));
 
 function App() {
     return (
         <React.Fragment>
-            <Switch>
-                <Route exact path="/">
-                    <MainPage />
-                </Route>
-                <Route exact path="/article/:id">
-                    <ArticlePage />
-                </Route>
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
+            <Suspense fallback={<Preloader />}>
+                <Switch>
+                    <Route exact path="/">
+                        <MainPage />
+                    </Route>
+                    <Route exact path="/article/:id">
+                        <ArticlePage />
+                    </Route>
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </Suspense>
         </React.Fragment>
     );
 }
