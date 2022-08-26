961936bb6c469780fc5adb48cc1b79052e261c0c
Roman Lesnoy <romanlesnoy@outlook.com>
2022-08-26 15:38:38 +0300

    Change getNews

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import Preloader from "./components/Preloader/Preloader";
+import { fetchNews } from "./store/news-actions";
 
 const MainPage = React.lazy(() => import("./pages/MainPage/MainPage"));
 const ArticlePage = React.lazy(() => import("./pages/ArticlePage/ArticlePage"));
 const NotFound = React.lazy(() => import("./pages/NotFound/NotFound"));
 
+let isInitial = true;
+
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (isInitial) {
+            dispatch(fetchNews());
+            isInitial = false;
+        }
+    }, [dispatch]);
+
     return (
         <React.Fragment>
             <Suspense fallback={<Preloader />}>
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,31 +1,17 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchNews } from "../../store/news-actions";
-import { newsActions } from "../../store/news-slice";
 
+import { newsActions } from "../../store/news-slice";
 import Header from "../../components/Header/Header";
 import NewsList from "../../components/NewsList/NewsList";
 import ErrorNotification from "../../components/ErrorNotification/ErrorNotification";
-
-let isInitial = true;
 
 const MainPage = () => {
     const dispatch = useDispatch();
     const notification = useSelector((state) => state.error.notification);
 
     useEffect(() => {
-        if (isInitial) {
-            dispatch(fetchNews());
-            isInitial = false;
-        }
-
-        // const interval = setInterval(() => {
-        //     dispatch(fetchNews());
-        // }, 60000);
-
         dispatch(newsActions.resetArticle());
-
-        // return () => clearInterval(interval);
     }, [dispatch]);
 
     return (
